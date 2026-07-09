dd481a27cc65e445ba887618f14abb6bc289448c
that1Kid <a.anaqi2010@gmail.com>
2026-07-09 22:33:29 +0800

    Fix: Row not found

--- src/errors.rs
+++ src/errors.rs
@@ -4,6 +4,9 @@ use sqlx::error::ErrorKind;
 
 pub async fn sqlx_error_handler(err: SqlError) -> AppError {
     match err {
+        SqlError::RowNotFound => AppError::NotFound {
+            message: "Item not found".to_string(),
+        },
         SqlError::Database(db_err) => match db_err.kind() {
             ErrorKind::NotNullViolation => AppError::BadRequest {
                 message: format!("Not null constraint violation: {}", db_err.message()),
